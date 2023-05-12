73e45f6d24ec3c68133ad0928f1c0f5e2e0f8546
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-05-12 20:32:23 +0530

    Added name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,14 @@ const Hero = () => {
           {/* added line below circle */}
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+        {/* For name after the circle */}
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Anindya</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop user interfaces <br className='sm:block hidden' /> and web application</p>
+        </div>
       </div>
+      {/* Rendering computer */}
+      <ComputersCanvas />
     </section>
   )
 }
