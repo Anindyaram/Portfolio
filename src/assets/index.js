527558bc15f657dd7ec60d05b4897aa4b5f3bdb9
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-05-13 16:08:32 +0530

    Added Experience section with the help of vertical timeline component

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import salesqueen from "./company/salesqueen.jpg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -53,6 +54,7 @@ export {
   typescript,
   threejs,
   meta,
+  salesqueen,
   shopify,
   starbucks,
   tesla,
